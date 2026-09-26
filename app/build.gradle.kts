e906db5b9b25cc3f4fe3b0f59a1165fcac714e29
jrs8205 <jarsi@jarsi.org>
2026-09-26 20:48:20 +0300

    Release 0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.jrs8205.appletvremote"
         minSdk = 34
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     signingConfigs {
